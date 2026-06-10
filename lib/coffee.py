2bac2d2abde3530dcfb74c69c6e4664cb3f6ccd2
adriankamunyu <adrianthegreatone7@gmail.com>
2026-06-10 12:02:36 +0300

    c

--- lib/coffee.py
+++ lib/coffee.py
@@ -17,5 +17,5 @@ class Coffee:
             self.__size = value
 
     def tip(self):
-        print("This coffee is great, here’s a tip")
+        print("This coffee is great, here’s a tip!")
         self.price += 1
